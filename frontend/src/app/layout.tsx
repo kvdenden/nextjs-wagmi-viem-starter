b8e11a0734d37c7165d1d4118a5402a8e922bdf9
Jan Van den Enden <jan@enden.be>
2024-05-19 21:04:47 +0200

    add site header with connect button

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
 import Providers from "./providers";
+import { SiteHeader } from "@/components/site-header";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
         )}
       >
         <Providers>
+          <SiteHeader />
           {children}
         </Providers>
       </body>
