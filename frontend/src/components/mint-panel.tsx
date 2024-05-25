b9e474c9d9b68adacf0a1fe2ed98fd21f645cd33
Jan Van den Enden <jan@enden.be>
2024-05-25 19:30:27 +0200

    implement public mint logic

--- frontend/src/components/mint-panel.tsx
+++ frontend/src/components/mint-panel.tsx
@@ -1,9 +1,9 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { MinusIcon, PlusIcon } from '@radix-ui/react-icons'
 import { useConnectModal } from '@rainbow-me/rainbowkit'
-import { useAccount, useBalance, useReadContracts, useWriteContract } from 'wagmi'
+import { useAccount, useBalance, useReadContracts, useWriteContract, useWaitForTransactionReceipt } from 'wagmi'
 import { signatureMinterAbi, nftAbi } from '@/web3/abi'
 import { CardDescription } from '@/components/ui/card'
 import { formatEther } from 'viem'
@@ -11,8 +11,9 @@ import { formatEther } from 'viem'
 export default function MintPanel() {
   const { isConnected, address } = useAccount()
   const { openConnectModal } = useConnectModal()
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(BigInt(1))
   const [mintHash, setMintHash] = useState<`0x${string}` | null>(null)
+  const [buttonText, setButtonText] = useState('Mint')
   const {
     data: balanceData,
     isError: balanceError,
@@ -34,6 +35,7 @@ export default function MintPanel() {
     data: mintData,
     isError: mintDataIsError,
     isLoading: mintDataIsLoading,
+    refetch: refetchMintData,
   } = useReadContracts({
     contracts: [
       { ...signatureMinterContract, functionName: 'getPublicSaleConfig' },
@@ -45,36 +47,57 @@ export default function MintPanel() {
   const publicSaleConfig = mintData && mintData[0].result
   const mintSupply = mintData && mintData[1].result
   const totalSupply = mintData && mintData[2].result
-  const price = publicSaleConfig && BigInt(quantity) * publicSaleConfig.unitPrice
+  const price = publicSaleConfig && quantity * publicSaleConfig.unitPrice
 
-  //mint logic:wip
-  const { data: hash, writeContract, error } = useWriteContract()
+  const { data: hash, writeContract, isPending, isError } = useWriteContract()
+  const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({
+    hash: mintHash as `0x${string}`,
+  })
 
   const handleMint = async () => {
     if (!publicSaleConfig) return
-    console.log('minting')
+    if (price && balanceData) {
+      if (price > balanceData.value) {
+        console.log('Insufficient balance', price > balanceData.value)
+        return
+      }
+    }
+    setButtonText('Sign in wallet')
     writeContract({
-      ...nftContract,
-      functionName: 'mint',
-      args: [address as `0x${string}`, BigInt(quantity)],
+      ...signatureMinterContract,
+      functionName: 'publicMint',
+      args: [quantity],
+      value: price as bigint,
     })
     setMintHash(hash as `0x${string}`)
   }
 
+  useEffect(() => {
+    if (isPending) {
+      setButtonText('Confirm in wallet')
+    } else if (isConfirming) {
+      setButtonText('Confirming transaction')
+    } else {
+      setButtonText('Mint')
+      setQuantity(BigInt(1))
+      refetchMintData()
+    }
+  }, [isConfirmed, isConfirming, isPending, isError])
+
   const handleMinus = () => {
     if (quantity > 1) {
-      setQuantity(quantity - 1)
+      setQuantity(quantity - BigInt(1))
     }
   }
 
   const handlePlus = () => {
     if (!publicSaleConfig) return
     if (quantity < publicSaleConfig.maxPerTransaction) {
-      setQuantity(quantity + 1)
+      setQuantity(quantity + BigInt(1))
     }
   }
 
-  if (mintDataIsLoading) {
+  if (mintDataIsLoading || isConfirming) {
     return (
       <Button className='w-full mt-6' disabled>
         Loading...
@@ -100,9 +123,8 @@ export default function MintPanel() {
   return (
     <div className='flex flex-col w-full'>
       <div className='flex justify-between my-2'>
-        {/* the first number needs to be updated */}
         <CardDescription className='my-2'>
-          {mintSupply ? mintSupply.toString() : '-'}/{mintSupply ? mintSupply.toString() : '-'}
+          {mintSupply && totalSupply ? totalSupply.toString() : '-'}/{mintSupply ? mintSupply.toString() : '-'}
         </CardDescription>
         <CardDescription className='my-2'>
           {publicSaleConfig ? formatEther(publicSaleConfig.unitPrice).toString() : '-'} ETH
@@ -110,14 +132,14 @@ export default function MintPanel() {
       </div>
       <div className='flex'>
         <div className='flex items-center justify-center gap-4 pr-4'>
-          <Button size='icon' variant='outline' onClick={handleMinus} disabled={quantity == 1}>
+          <Button size='icon' variant='outline' onClick={handleMinus} disabled={quantity == BigInt(1)}>
             <MinusIcon className='h-4 w-4' />
           </Button>
-          <span className='text-xl font-semibold'>{quantity}</span>
+          <span className='text-xl font-semibold'>{quantity.toString()}</span>
           <Button
             size='icon'
             variant='outline'
-            disabled={publicSaleConfig && quantity == publicSaleConfig.maxPerTransaction}
+            disabled={publicSaleConfig && quantity == BigInt(publicSaleConfig.maxPerTransaction)}
             onClick={handlePlus}
           >
             <PlusIcon className='h-4 w-4' />
@@ -126,9 +148,9 @@ export default function MintPanel() {
         <Button
           onClick={handleMint}
           className='w-full'
-          disabled={price && balanceData ? price > balanceData?.value : true}
+          disabled={price && balanceData ? price > balanceData?.value || isConfirming : true}
         >
-          Mint
+          {buttonText}
         </Button>
       </div>
     </div>
